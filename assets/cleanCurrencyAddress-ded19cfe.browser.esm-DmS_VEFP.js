2afe3376f27c02dfa0686fc6b418935b27a628ab
Joyal George Joseph <64003591+JoyalGJ@users.noreply.github.com>
2025-03-14 10:22:55 +0000

    Updates

---
+++ assets/cleanCurrencyAddress-ded19cfe.browser.esm-DmS_VEFP.js
@@ -0,0 +1,1 @@
+import{bz as a,aM as e}from"./index-BzFxWc_h.js";function t(r){return a(r)?e:r}export{t as c};
---
+++ assets/hasERC20Allowance-654a940b.browser.esm-CDLYmPrd.js
@@ -0,0 +1,2 @@
+const __vite__mapDeps=(i,m=__vite__mapDeps,d=(m.f||(m.f=["assets/index-BzFxWc_h.js","assets/index-97gOCaEy.css"])))=>i.map(i=>d[i]);
+import{f as _,a1 as c}from"./index-BzFxWc_h.js";async function f(e,a,t){const n=e.getProvider(),r=(await _(async()=>{const{default:o}=await import("./index-BzFxWc_h.js").then(l=>l.eS);return{default:o}},__vite__mapDeps([0,1]))).default,s=new c(n,a,r,{},e.storage),d=await e.getSignerAddress(),i=e.address;return(await s.read("allowance",[d,i])).gte(t)}export{f as h};
